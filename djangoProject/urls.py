e3b38e708964f26adde5b9c49a7ca41e36d22c1e
joons <joonamm@naver.com>
2021-03-26 23:46:59 +0900

    add_app

---
+++ djangoProject/urls.py
@@ -0,0 +1,25 @@
+"""djangoProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from .views import index
+
+urlpatterns = [
+    path('polls/', include('polls.urls')), # polls에 있는 것들을 잡아내서 poll의 url을 전달
+    path('admin/', admin.site.urls),
+    path('',index)
+    # path( 주소, 뷰, 주소의 별명 )
+]
