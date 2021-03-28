a83204938c4a47f9596730f01fc41ec5af5d9fa3
joons <joonamm@naver.com>
2021-03-28 17:20:27 +0900

    chapter4

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
 
 from django.http import Http404
 from django.template import loader
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -16,16 +18,29 @@ def index(request):
     }
     # return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', context)
+
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question dose not exist")
-    return render( request, 'polls/detail.html', {'question':question})
-    return HttpResponse("You're looking at question %s." % question_id)
+    return render( request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the result "
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voirnu")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question':question,
+            'error_message':"You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,))) # polls/3/results
+
