f0ced6845b22a3eae19ff9db39a638fb48d15d1b
joons <joonamm@naver.com>
2021-04-02 23:27:37 +0900

    admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,4 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin,StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ['question_text']}),
+        ('Date information', {'fields':['pub_date'], "classes" : ['collapse']})
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+
+
+## Choice 페이지 관련 admin 게정만들기
