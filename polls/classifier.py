fbe096ab91ae1ab491acb5c96b8099807898d80b
joons <joonamm@naver.com>
2021-04-07 23:45:22 +0900

    modeling

--- polls/classifier.py
+++ polls/classifier.py
@@ -5,10 +5,11 @@ class Model(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = resnet34(pretrained=True)
-        #         self.model = timm.create_model('inception_resnet_v2', pretrained = True)
         self.model.fc = nn.Linear(512, 18)
-
-    #         self.model.classif = nn.Linear(1536, 18)
+        self.fc = nn.Sequential( nn.Linear(256,18), 
+                                nn.ReLU(),
+                                nn.Dropout(0.1),
+        )
 
     def forward(self, x):
         x = self.model(x)
